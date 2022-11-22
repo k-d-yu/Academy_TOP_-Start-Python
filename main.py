3dc45a5fa782f7bb23388f7b5d48182565d07cd3
k-d-yu <dima_kul95@mail.ru>
2022-11-22 21:43:43 +0300

    reg

--- main.py
+++ main.py
@@ -3089,7 +3089,7 @@ import re
 # print(re.findall(reg, s))
 
 # s = "5 + 7*2 - 4"
-# reg = r"\s*([+*-])\s*"  # ['5', '+', '7', '*', '2', '-', '4']
+# # reg = r"\s*([+*-])\s*"  # ['5', '+', '7', '*', '2', '-', '4']
 # # reg = r"\s*[+*-]\s*" ['5', '7', '2', '4']
 # print(re.split(reg, s))
 
@@ -3101,6 +3101,38 @@ import re
 # print(m[2])
 # print(m[0])
 
-# print("Hello")
 
-print("New Project")
+# text = """
+# Самара
+# Москва
+# Тверь
+# Уфа
+# Казань
+# """
+# count = 0
+#
+#
+# def replace_find(m):
+#     global count
+#     count += 1
+#     return f"<option value='{count}'>{m.group(1)}</option>\n"
+#
+#
+# print(re.sub(r"\s*(\w+)\s*", replace_find, text))
+
+# s = "<p>Изображение <img src='bg.jpg'> - фон страницы</p>"
+# reg = r"<img\s+[^>]*src=([\"'])(.+)\1>"
+# print(re.findall(reg, s))
+
+# s = "<p>Изображение <img src='bg.jpg'> - фон страницы</p>"
+# reg = r"<img\s+[^>]*src=(?P<q>[\"'])(.+)(?P=q)>"
+# print(re.findall(reg, s))
+
+# s = "Самолет прилетает 10/23/2023. Будем рады вас видеть после 10/24/2023."
+# reg = r"(\d{2})/(\d{2})/(\d{4})"
+# print(re.findall(reg, s))
+# print(re.sub(reg, r"\2.\1", s))
+
+# s = "google.com and google.ru"
+# reg = r"(([a-z0-9-]{2,}\.)+[a-z]{2,4})"
+# print(re.sub(reg, r"http://\1", s))
